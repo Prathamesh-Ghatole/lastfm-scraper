9d1fa9e33f971808ce73ec4ed2d4e68868fae468
varmor <morevarun4004@gmail.com>
2022-02-23 15:02:46 +0530

    setting up flask

---
+++ webserver/__init__.py
@@ -0,0 +1,10 @@
+from distutils.log import debug
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+app.config['SECRET_KE']= "SuperSecure"
+
+from .views import views
+app.register_blueprint(views, url_prefix="/")
+
+
+
