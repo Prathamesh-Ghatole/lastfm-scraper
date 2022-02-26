7434e19b730ab440f947e359d8ce872dda245166
Prathamesh-Ghatole <prathamesh.s.ghatole@gmail.com>
2022-02-26 23:00:21 +0530

    modified main.py to default state

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-import auth
-
-print("Initializing main.py!")
-print("Initiating API authorization")
-print()
-
-#authenticating & Loading ConfigClass object in variable config
-config = auth.initialize()
-
-
-#printing authenticated credentials
-print("useragent: {}".format(config.useragent))
-print("username: {}".format(config.username))
-print("keylist: {}".format(config.keylist))
-print()
-for i in range(7):
-    print(config.getKey())
+#Empty for now ##Main.py
