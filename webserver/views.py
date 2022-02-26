df424ebc886289ad8c320a36eddc04393eae985e
varmor <morevarun4004@gmail.com>
2022-02-27 00:00:43 +0530

    Removed test case from server

--- webserver/views.py
+++ webserver/views.py
@@ -5,18 +5,12 @@ import sys
 
 views = Blueprint("views", __name__)
 
-def demo(username):
-    config = auth.initialize()
-    lastfm.initCache()
-    response = lastfm.getReq(key=config.getKey(), useragent=config.useragent, user=username, method='artist.getInfo', load={'artist':'Taylor Swift'})
-    return response.json()['artist']
-
 @views.route("/", methods=["POST", "GET"])
 def index():
     if request.method == "POST":
         username = request.form["Username"]
         type = request.form["file_type"]
-        print(demo(username=username))
+        
         print(username)
         print(type)
     return render_template("base.html")
