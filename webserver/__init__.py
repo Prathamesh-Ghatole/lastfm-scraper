51e6c7dc984878c718142b5f4a5d347250a84540
varmor <morevarun4004@gmail.com>
2022-04-04 10:01:10 +0530

    Added Download Functionality

--- webserver/__init__.py
+++ webserver/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
-app.config['SECRET_KE']= "SuperSecure"
+app.config['SECRET_KEY']= "SuperSecure"
 
 from .views import views
 app.register_blueprint(views, url_prefix="/")
--- webserver/views.py
+++ webserver/views.py
@@ -1,41 +1,49 @@
+from email import message
 from flask import Blueprint, render_template, request, send_file, redirect, url_for, flash
 import lib.auth as auth
 import lib.lastfm as lastfm
 import sys
 
 views = Blueprint("views", __name__)
+dictt ={
+    "username": None,
+    "type": None
+}
 
 @views.route("/", methods=["POST", "GET"])
 def index():
     config = auth.init()
     lastfm.initCache()
+    flash("Click on ")
     
     if request.method == "POST":
-        username = request.form["Username"]
-        config.username=username
-        type = request.form["file_type"]
-        
-        print("Fetching Scrobbles:")
-        scrobbles = lastfm.grab(config)
-        print("Done!\n")
-
-        if type != 'JSON' or type != 'CSV':
-            type = 'ALL'
-
-        lastfm.export(scrobbles, type)
-        
-        return redirect(url_for("views.download"))
+        dictt["username"] = request.form["Username"]
+        config.username=dictt["username"]
+        dictt["type"] = request.form["file_type"]
+        type = dictt["type"]
+                
+        return render_template("base.html", config = config, type = type)
 
     return render_template("base.html")
 
 
-@views.route("/download")
-def download():
-    if type == 'JSON':
-        path = "E:\Projects\lastfm-scraper\exports\export.json"
-    elif type == "CSV":
-        path = "E:\Projects\lastfm-scraper\exports\export.csv"
-    else:
-        path = "E:\Projects\lastfm-scraper\exports\export.csv"
+@views.route("/download/<type>")
+def download(type):
+    config = auth.init()
+    lastfm.initCache()
+    print("Fetching Scrobbles:")
+    scrobbles = lastfm.grab(config)
+    print("Done!\n")
 
-    return send_file(path, as_attachment = True, )
+    # if type != 'JSON' or type != 'CSV':
+    #     type = 'ALL'
+    lastfm.export(scrobbles, type)
+
+    print(dictt["type"])
+    if dictt["type"] == 'JSON':
+        path = ".\..\exports\export.json"
+    elif dictt["type"] == "CSV":
+        path = ".\..\exports\export.csv"
+    send_file(path, as_attachment = True )
+    return send_file(path, as_attachment = True )
+
